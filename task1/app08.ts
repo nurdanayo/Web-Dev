b893bc37be924c9883a320dac681771f23f6bdd7
nurdanayo <turlybekovanurdana2006@gmail.com>
2026-02-25 15:25:38 +0500

    Delete task1 directory

--- task1/app08.ts
+++
@@ -1,9 +0,0 @@
-import {Component, input} from '@angular/core';
-
-@Component({
-  selector: 'app-user',
-  template: ` <p>The user's name is {{ name() }}</p> `,
-})
-export class User {
-    readonly name = input<string>();
-}
